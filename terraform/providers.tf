d38f261996be735b446e176ee6f1faf2d89b3a06
Lyubomir <lubohero@abv.bg>
2024-01-04 13:22:20 +0200

    Setup Terraform Cloud configuration

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,22 +1,19 @@
 terraform {
   required_version = ">=1.0"
-
   required_providers {
-    azapi = {
-      source  = "azure/azapi"
-      version = "~>1.5"
-    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-    time = {
-      source  = "hashicorp/time"
-      version = "0.9.1"
+  }
+}
+
+terraform {
+  cloud {
+    organization = "Project-DevOps-Piss"
+
+    workspaces {
+      name = "terraform-cloud-piss"
     }
   }
 }
